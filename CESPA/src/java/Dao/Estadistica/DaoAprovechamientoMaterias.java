a8b302aa03aa5b87383dba687986e21f2021d850
alezk <alejandro.rivera@morelos.gob.mx>
2016-02-04 16:42:24 -0600

    Modificación

--- CESPA/src/java/Dao/Estadistica/DaoAprovechamientoMaterias.java
+++ CESPA/src/java/Dao/Estadistica/DaoAprovechamientoMaterias.java
@@ -23,8 +23,11 @@ public class DaoAprovechamientoMaterias {
     public static String SqlMateriasPresentadas = "select c.clave_materia, m.materia_dsc, count(*) from nuples_calificaciones c, nuples_modulos m where m.clave_materia=c.clave_materia and clave_etapa=? group by c.clave_materia,m.materia_dsc order by c.clave_materia";
     public static String SqlMateriasAcreditadas = "select c.clave_materia, m.materia_dsc, count(*) from nuples_calificaciones c, nuples_modulos m where m.clave_materia=c.clave_materia and clave_etapa=? and calificacion > 5 group by c.clave_materia,m.materia_dsc order by c.clave_materia ";
 
-    public static String SqlMateriasPresentadas33 = "select c.clave_materia, m.materia_dsc, count(*) from calificaciones c, materias m where m.clave_materia=c.clave_materia and clave_etapa=? group by c.clave_materia,m.materia_dsc order by c.clave_materia";
+    public static String SqlMateriasPresentadas33 = "select c.clave_materia, m.materia_dsc, count(*) from calificaciones c, materias m where m.clave_materia=c.clave_materia and clave_etapa=?  group by c.clave_materia,m.materia_dsc order by c.clave_materia";
     public static String SqlMateriasAcreditadas33 = "select c.clave_materia, m.materia_dsc, count(*) from calificaciones c, materias m where m.clave_materia=c.clave_materia and clave_etapa=? and calificacion > 5 group by c.clave_materia,m.materia_dsc order by c.clave_materia";
+
+    public static String SqlMateriasPresentadas33Ex = "select c.clave_materia, m.materia_dsc, count(*) from calificaciones c, materias m where m.clave_materia=c.clave_materia and clave_etapa=? and c.fase in('1','2','3','4','5') group by c.clave_materia,m.materia_dsc order by c.clave_materia";
+    public static String SqlMateriasAcreditadas33Ex = "select c.clave_materia, m.materia_dsc, count(*) from calificaciones c, materias m where m.clave_materia=c.clave_materia and clave_etapa=? and c.fase in('1','2','3','4','5') and calificacion > 5 group by c.clave_materia,m.materia_dsc order by c.clave_materia";
 
     public List<BeanMaterias> MateriasAcreditadas(int etapa, String fase, String plan) {
         List<BeanMaterias> lista = new ArrayList();
@@ -35,8 +38,10 @@ public class DaoAprovechamientoMaterias {
 
             if (plan.equals("p22")) {
                 ps = connect.prepareStatement(SqlMateriasAcreditadas);
-            } else {
+            } else if (plan.equals("p33")) {
                 ps = connect.prepareStatement(SqlMateriasAcreditadas33);
+            } else {
+                ps = connect.prepareStatement(SqlMateriasAcreditadas33Ex);
             }
 
             ps.setInt(1, etapa);
@@ -68,13 +73,15 @@ public class DaoAprovechamientoMaterias {
 
             if (plan.equals("p22")) {
                 ps = connect.prepareStatement(SqlMateriasPresentadas);
-            } else {
+            } else if (plan.equals("p33")) {
                 ps = connect.prepareStatement(SqlMateriasPresentadas33);
+            } else {
+                ps = connect.prepareStatement(SqlMateriasPresentadas33Ex);
             }
 
             ps.setInt(1, etapa);
 //            ps.setString(2, fase);
-         
+
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
